6392add3ce418926abb5436718fdc51831d65c23
rubyfaye00 <rubyfaye00@gmail.com>
2025-09-28 19:15:11 -0400

    Add files via upload

---
+++ Villanname.py
@@ -0,0 +1,8 @@
+from datetime import date
+def get_villain_name(birthdate): 
+    first = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
+    last = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"]
+    return first[birthdate.month - 1] + ' ' + last[int(str(birthdate.day)[-1])]
+
+birthday = date(2006, 11, 20)  
+print(get_villain_name(birthday))

09205afaa507422fa39fb7ce9d6e30d63acbf97f
rubyfaye00 <rubyfaye00@gmail.com>
2025-10-05 22:44:16 -0400

    archersready.py

--- archersready.py
+++ archersready.py
@@ -1,6 +1,12 @@
+#Every Archer Has its Arrows
+#https://www.codewars.com/kata/559f89598c0d6c9b31000125
+#This code checks if the archers have at least 5 arrows.
+#Returns true if the list is empty and every value that is 5 or greater
+#returns false if archers have fewer than 5 arrows 
 def archers_ready(archers):
     return len(archers) > 0 and all(arrows >= 5 for arrows in archers)
 
 print(archers_ready([1, 2, 3, 4, 5, 6, 7, 8])) 
 print(archers_ready([5, 6, 7, 8]))             
 print(archers_ready([]))                       
+

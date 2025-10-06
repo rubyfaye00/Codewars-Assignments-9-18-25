76009a016d4fb241d7aca1ec46ac88057f3665fb
rubyfaye00 <rubyfaye00@gmail.com>
2025-10-05 22:20:40 -0400

    Villanname.py

--- Villanname.py
+++ Villanname.py
@@ -1,3 +1,9 @@
+#Find Your Villan Name
+#https://www.codewars.com/kata/536c00e21da4dc0a0700128b
+#This code generates a Villan Name based of of your birthday.
+#Uses the birth month to select an adjective from a list and the last digit of the birth day to choose a noun.
+#This combined creates the final name, I used my birthday Novemeber 20th to get "The Terrifying Mustache"
+
 from datetime import date
 def get_villain_name(birthdate): 
     first = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
@@ -6,3 +12,4 @@ def get_villain_name(birthdate):
 
 birthday = date(2006, 11, 20)  
 print(get_villain_name(birthday))
+

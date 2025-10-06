c468a4d19c6eed4525d57b1d60bd68bc990be44b
rubyfaye00 <rubyfaye00@gmail.com>
2025-10-05 22:36:06 -0400

    Uno.py

--- Uno.py
+++ Uno.py
@@ -1,3 +1,7 @@
+#Uno Match Play 
+#https://www.codewars.com/kata/687dd1e199ab3955339b8071
+#This code checks if any card in a players hand can be played on the current face up card.
+#It compares the cards color and the number to the faceup card if either matches it returns true if none it returns false
 def can_play(hand, face_up):
     
     if not hand:
@@ -19,3 +23,4 @@ print(can_play(["yellow 3", "yellow 5", "red 8"], "blue 5"))
 print(can_play(["yellow 3", "blue 5", "red 8", "red 9"], "green 4"))            
 print(can_play(["yellow 3", "red 8"], "green 2"))                               
 print(can_play([], "red 5"))                                                    
+

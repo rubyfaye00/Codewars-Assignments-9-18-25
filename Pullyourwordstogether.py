f79271bf6c40232be5dde9145a22caaf8e7710a2
rubyfaye00 <rubyfaye00@gmail.com>
2025-10-05 22:40:03 -0400

    Pullyourwordstogether.py

--- Pullyourwordstogether.py
+++ Pullyourwordstogether.py
@@ -1,3 +1,7 @@
+#Pull Your Words Together Man!
+#https://www.codewars.com/kata/59ad7d2e07157af687000070
+#This code turns a list of words into a sentence.
+#It capitalizes the first word, joins all the words, and adds a period at the end.
 def sentencify(words):
     if not words:
         return ""
@@ -12,4 +16,5 @@ def sentencify(words):
 
 print(sentencify(["this", "is", "a", "sentence"]))
 print(sentencify(["please", "help", "me"]))
+
 print(sentencify(["dont", "look", "in", "the", "basement"]))
